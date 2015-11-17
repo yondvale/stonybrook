65377eadc698783efa5f8d319b7e4a49baf5caca
yondvale <xavier@yondvale.com>
2015-11-17 10:35:06 -0500

    minus slider

--- sketch.js
+++ sketch.js
@@ -5,19 +5,19 @@ var slider;
 function setup() {
  createCanvas(windowWidth, windowHeight);
  strokeWeight(4);
- colorMode(HSB, 255);
- slider = createSlider(0, 255, 127);
- slider = createSlider(0, 255, 100);
- slider.position(10,10);
- slider.style('width', '80px');
+ //colorMode(HSB, 255);
+// slider = createSlider(0, 255, 127);
+// slider = createSlider(0, 255, 100);
+// slider.position(10,10);
+// slider.style('width', '80px');
   stroke('orange');
   frameRate(10);
 } 
 
 function draw() {
-    var val = slider.value();
+//    var val = slider.value();
   background(0);
-  stroke(val);
+  stroke('orange');
   var rand = 0;
   for  (var i = 1; i < steps; i++) {
     point( (width/steps) * i, (height/2) + random(-rand, rand) );
